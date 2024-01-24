8f295b7e29e70f0dd7346c07edc2793c11cbdb09
sejin park <seijin0722@naver.com>
2024-01-24 20:48:05 +0900

    feat : initial commit

---
+++ src/main/java/com/flab/offcoupon/OffcouponApplication.java
@@ -0,0 +1,13 @@
+package com.flab.offcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffcouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffcouponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/offcoupon/OffcouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.offcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffcouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
