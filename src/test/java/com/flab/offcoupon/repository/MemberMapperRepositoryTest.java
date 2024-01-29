c1d4f17b18a8ba8f9c5d27211ebd7a57598c18f3
sejin park <seijin0722@naver.com>
2024-01-29 17:17:26 +0900

    test : member repository 회원가입 테스트

--- src/test/java/com/flab/offcoupon/repository/MemberMapperRepositoryTest.java
+++ src/test/java/com/flab/offcoupon/repository/MemberMapperRepositoryTest.java
@@ -4,22 +4,28 @@ import com.flab.offcoupon.controller.api.MemberMapperDTO;
 import com.flab.offcoupon.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+
 import org.springframework.transaction.annotation.Transactional;
 
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@MybatisTest
 class MemberMapperRepositoryTest {
+
     @Autowired
     private MemberMapperRepository memberMapperRepository;
     @Test
     @Transactional
-    @DisplayName("Mybatis 회원가입 테스트")
-    public void userSaveTest() {
+    @DisplayName("[SUCCESS] 회원 가입 성공")
+    public void save() {
+        // given
         MemberMapperDTO mapperDTO = MemberMapperDTO.create("test",",1234", "name", "20021223", "01075805060");
         Member entity = Member.toEntity(mapperDTO);
+        //when
         memberMapperRepository.save(entity);
     }
-
 }
