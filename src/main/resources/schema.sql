82d11616637b54207f4df03cec197e81ea6e2ca1
sejin park <seijin0722@naver.com>
2024-02-27 16:06:20 +0900

    feat : 쿠폰 발행 이벤트 엔티티 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,55 @@
 DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS coupon;
+DROP TABLE IF EXISTS event;
+DROP TABLE IF EXISTS coupon_issue;
 
 CREATE TABLE member
 (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '회원 식별자',
-    email VARCHAR(40) NOT NULL COMMENT '회원 이메일',
-    password VARCHAR(255) NOT NULL COMMENT '비밀번호',
-    name VARCHAR(40) NOT NULL COMMENT '회원 이름',
-    birthDate VARCHAR(40) NOT NULL COMMENT '생년월일',
-    phone VARCHAR(40) NOT NULL COMMENT '휴대폰 번호',
-    createdAt TIMESTAMP NOT NULL COMMENT '데이터 생성일',
-    updatedAt TIMESTAMP NOT NULL COMMENT '데이터 변경일'
+    id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '회원 식별자',
+    email      VARCHAR(40)  NOT NULL COMMENT '회원 이메일',
+    password   VARCHAR(255) NOT NULL COMMENT '비밀번호',
+    name       VARCHAR(40)  NOT NULL COMMENT '회원 이름',
+    birthdate  DATETIME     NOT NULL COMMENT '생년월일',
+    phone      VARCHAR(40)  NOT NULL COMMENT '휴대폰 번호',
+    role       VARCHAR(50)  NOT NULL COMMENT '회원 권한',
+    created_at DATETIME     NOT NULL COMMENT '데이터 생성일',
+    updated_at DATETIME     NOT NULL COMMENT '데이터 변경일'
+);
+
+CREATE TABLE event
+(
+    event_id               BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '이벤트 식별자',
+    category               VARCHAR(100) NOT NULL COMMENT '이벤트 카테고리',
+    description            VARCHAR(255) NOT NULL COMMENT '이벤트 설명',
+    start_date             DATE         NOT NULL COMMENT '이벤트 시작일',
+    end_date               DATE         NOT NULL COMMENT '이벤트 종료일',
+    daily_issue_start_time VARCHAR(20)  NULL COMMENT '당일 쿠폰 발행 시작시간 e.g. "13:00:00"/ null일 경우 무한 발행',
+    daily_issue_end_time   VARCHAR(20)  NULL COMMENT '당일 쿠폰 발행 종료시간 e.g. "15:00:00" / null일 경우 무한 발행'
+);
+
+CREATE TABLE `coupon`
+(
+    id                  BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '쿠폰 식별자',
+    discount_type       VARCHAR(40)     NOT NULL COMMENT '정액, 정률 등',
+    discount_rate       BIGINT UNSIGNED NULL COMMENT '정률 할인',
+    discount_price      BIGINT UNSIGNED NULL COMMENT '정액 할인',
+    validate_start_date DATETIME        NOT NULL COMMENT '모든 쿠폰은 유효 시간이 있어야한다는 제약 사항 존재',
+    validate_end_date   DATETIME        NOT NULL COMMENT '모든 쿠폰은 유효 시간이 있어야한다는 제약 사항 존재',
+    coupon_type         VARCHAR(50)     NOT NULL COMMENT '선착순 쿠폰, 회원가입 쿠폰 등..',
+    status              VARCHAR(50)     NOT NULL DEFAULT 'NOT_ACTIVE' COMMENT '유효일 전 : NOT_ACTIVE / 유효기간 : ACTIVE / 사용완료 : USED / 만료 : EXPIRED',
+    max_quantity        BIGINT UNSIGNED NULL COMMENT '무제한 발행일 경우 NULL',
+    issued_quantity     BIGINT UNSIGNED NULL COMMENT '무제한 발행일 경우 NULL',
+    created_at          DATETIME        NOT NULL COMMENT '데이터 생성일',
+    updated_at          DATETIME        NOT NULL COMMENT '데이터 변경일',
+    event_id            BIGINT UNSIGNED NULL COMMENT '비식별 관계 / null이어도 쿠폰 발급 가능(e.g. 회원가입))'
+);
+
+CREATE TABLE coupon_issue
+(
+    coupon_issue_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '쿠폰 발행 기록',
+    member_id       BIGINT UNSIGNED NOT NULL COMMENT '쿠폰 ID',
+    coupon_id       BIGINT UNSIGNED NOT NULL COMMENT '회원 ID',
+    isUsed          VARCHAR(255)    NOT NULL COMMENT '사용 여부',
+    created_at      DATETIME        NOT NULL COMMENT '데이터 생성일',
+    updated_at      DATETIME        NOT NULL COMMENT '데이터 변경일'
 );
