3c82ef269b4edfd89df18425f25d27ec649d361e
sejin park <seijin0722@naver.com>
2024-04-09 10:51:20 +0900

    refactor : 스키마 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,11 +8,11 @@ DROP TABLE IF EXISTS order_detail;
 CREATE TABLE member
 (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '회원 식별자',
-    email VARCHAR(40) NOT NULL COMMENT '회원 이메일',
+    email VARCHAR(255) NOT NULL COMMENT '회원 이메일',
     password VARCHAR(255) NOT NULL COMMENT '비밀번호',
-    name VARCHAR(40) NOT NULL COMMENT '회원 이름',
+    name VARCHAR(100) NOT NULL COMMENT '회원 이름',
     birthdate DATE NOT NULL COMMENT '생년월일',
-    phone VARCHAR(40) NOT NULL COMMENT '휴대폰 번호',
+    phone VARCHAR(50) NOT NULL COMMENT '휴대폰 번호',
     role VARCHAR(50) NOT NULL COMMENT '회원 권한',
     created_at DATETIME NOT NULL COMMENT '데이터 생성일',
     updated_at DATETIME NOT NULL COMMENT '데이터 변경일'
@@ -35,10 +35,10 @@ CREATE TABLE coupon
 (
     id                  BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '쿠폰 식별자',
     event_id            BIGINT UNSIGNED NULL COMMENT '이벤트 식별자 / NULL일 경우 이벤트와 관련 없는 쿠폰(e.g. 회원가입 쿠폰)',
-    discount_type       VARCHAR(40)     NOT NULL COMMENT '정액, 정률 등',
+    discount_type       VARCHAR(50)     NOT NULL COMMENT '정액, 정률 등',
     discount_rate       BIGINT UNSIGNED NULL COMMENT '정률 할인',
     discount_price      BIGINT UNSIGNED NULL COMMENT '정액 할인',
-    coupon_type         VARCHAR(50)     NOT NULL COMMENT '선착순 쿠폰, 회원가입 쿠폰 등..',
+    coupon_type         VARCHAR(100)     NOT NULL COMMENT '선착순 쿠폰, 회원가입 쿠폰 등..',
     max_quantity        BIGINT UNSIGNED NULL COMMENT '무제한 발행일 경우 NULL',
     issued_quantity     BIGINT UNSIGNED NULL COMMENT '무제한 발행일 경우 NULL',
     validate_start_date DATETIME        NOT NULL COMMENT '모든 쿠폰은 유효 시간이 있어야한다는 제약 사항 존재',
@@ -61,8 +61,8 @@ CREATE TABLE coupon_issue_no_idx
 CREATE TABLE product
 (
     id  BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '상품 식별자',
-    category VARCHAR(100) NOT NULL COMMENT '상품 카테고리',
-    title VARCHAR(100) NOT NULL COMMENT '상품명',
+    category VARCHAR(255) NOT NULL COMMENT '상품 카테고리',
+    title VARCHAR(255) NOT NULL COMMENT '상품명',
     description VARCHAR(255) NOT NULL COMMENT '상품 설명',
     original_price BIGINT UNSIGNED NOT NULL COMMENT '원래 상품 가격',
     sale_price BIGINT UNSIGNED NULL COMMENT '할인 상품 가격(쿠폰과 관계없이 전체적으로 할인할 경우)',
@@ -89,7 +89,7 @@ CREATE TABLE order_detail
 CREATE TABLE order_coupon (
   id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT '쿠폰을 사용한 주문 식별자',
   order_id BIGINT UNSIGNED NOT NULL COMMENT '주문 ID',
-  coupon_id VARCHAR(50) NOT NULL COMMENT '쿠폰 ID',
+  coupon_id BIGINT UNSIGNED NOT NULL COMMENT '쿠폰 ID',
   discount_amount BIGINT UNSIGNED NOT NULL COMMENT '쿠폰 할인액',
   created_at DATETIME NOT NULL COMMENT '데이터 생성일',
   updated_at DATETIME NOT NULL COMMENT '데이터 변경일'
