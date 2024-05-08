e6fd16344c7db6bd5856423fa27dd64aca0bf014
sejin park <seijin0722@naver.com>
2024-05-08 16:24:38 +0900

    test : 레빗엠큐 테스트 컨테이너 일단커밋 - 실패

--- src/test/java/com/flab/offcoupon/AbstractIntegrationContainerBaseTest.java
+++ src/test/java/com/flab/offcoupon/AbstractIntegrationContainerBaseTest.java
@@ -1,76 +1,60 @@
 package com.flab.offcoupon;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
-//@ActiveProfiles("test")
+@ContextConfiguration
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class AbstractIntegrationContainerBaseTest {
 //    @Value("${DB_DATABASE}")
 //    private static String database;
 
+//    @LocalServerPort
+//    int port;
     private static final Logger logger = LoggerFactory.getLogger(AbstractIntegrationContainerBaseTest.class);
-    private static final GenericContainer MY_REDIS_CONTAINER;
-    //private static final MySQLContainer MY_SQL_CONTAINER;
-    private static final RabbitMQContainer MY_RABBITMQ_CONTAINER;
+    @Container
+    static GenericContainer MY_REDIS_CONTAINER =
+            new GenericContainer("redis:5.0.3-alpine")
+                    .withExposedPorts(6379)
+                    .withEnv("REDIS_PASSWORD", "yourRedisPassword"); // 비밀번호 설정
+//    @Container
+//    static MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:8");
+    @Container
+    static RabbitMQContainer MY_RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.10.7");
 
-    static {
-        // 레디스 컨테이너
-        MY_REDIS_CONTAINER = new GenericContainer<>("redis:6")
-                .withExposedPorts(6379)
-                .withReuse(true);
-        MY_REDIS_CONTAINER.start();
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", MY_REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> MY_REDIS_CONTAINER.getMappedPort(6379));
+        registry.add("spring.data.redis.password", () -> MY_REDIS_CONTAINER.getEnvMap().get("REDIS_PASSWORD"));
 
-        logger.info(">> MY_REDIS_CONTAINER.getHost() = " + MY_REDIS_CONTAINER.getHost());
-        logger.info(">> MY_REDIS_CONTAINER.getMappedPort(6379) = " + MY_REDIS_CONTAINER.getMappedPort(6379));
-        logger.info(">> MY_REDIS_CONTAINER.getContainerIpAddress() = " + MY_REDIS_CONTAINER.getContainerIpAddress());
+//        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+//        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+//        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
 
-//        System.setProperty("spring.redis.host", MY_REDIS_CONTAINER.getHost());
-//        System.setProperty("spring.redis.port", MY_REDIS_CONTAINER.getMappedPort(6379).toString());
-//        System.setProperty("spring.redis.password", "");
-
-        // 레빗엠큐 컨테이너
-        MY_RABBITMQ_CONTAINER = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3.11.11-alpine"));
-        MY_RABBITMQ_CONTAINER.start();
-
-        logger.info(">> MY_RABBITMQ_CONTAINER.getHose() = " + MY_RABBITMQ_CONTAINER.getHost());
-        logger.info(">> MY_RABBITMQ_CONTAINER.getMappedPort(5672) = " + MY_RABBITMQ_CONTAINER.getMappedPort(5672));
-        logger.info(">> MY_RABBITMQ_CONTAINER.getContainerIpAddress() = " + MY_RABBITMQ_CONTAINER.getContainerIpAddress());
-
-//        System.setProperty("spring.rabbitmq.host", MY_RABBITMQ_CONTAINER.getHost());
-//        System.setProperty("spring.rabbitmq.port", MY_RABBITMQ_CONTAINER.getMappedPort(5672).toString());
-//        System.setProperty("spring.rabbitmq.username", "guest");
-//        System.setProperty("spring.rabbitmq.password", "guest");
-
-
-        //RabbitMQTestInitializer.initializeRabbitMQ(MY_RABBITMQ_CONTAINER);
+        registry.add("spring.rabbitmq.host", MY_RABBITMQ_CONTAINER::getHost);
+        registry.add("spring.rabbitmq.port", () -> MY_RABBITMQ_CONTAINER.getMappedPort(5672));
+        registry.add("spring.rabbitmq.username", MY_RABBITMQ_CONTAINER::getAdminUsername);
+        registry.add("spring.rabbitmq.password", MY_RABBITMQ_CONTAINER::getAdminPassword);
     }
 
-//    @BeforeEach
-//    void setUpRabbitMQ() {
-//        RabbitMQTestInitializer.initializeRabbitMQ(MY_RABBITMQ_CONTAINER);
-//    }
+    @BeforeEach
+    void setUpRabbitMQ() {
+        RabbitMQTestInitializer.initializeRabbitMQ(MY_RABBITMQ_CONTAINER);
+    }
 }
-
-
-//        MY_SQL_CONTAINER = new MySQLContainer<>("mysql:8.0.25")
-//                .withDatabaseName("off-coupon")
-//                .withUsername("root")
-//                .withPassword("");
-//        MY_SQL_CONTAINER.start();
-////
-//        logger.info(">> MY_SQL_CONTAINER.getDatabaseName() = " + MY_SQL_CONTAINER.getDatabaseName());
-//        logger.info(">> MY_SQL_CONTAINER.getJdbcUrl() = " + MY_SQL_CONTAINER.getJdbcUrl());
-//        logger.info(">> MY_SQL_CONTAINER.getUsername() = " + MY_SQL_CONTAINER.getUsername());
-//        logger.info(">> MY_SQL_CONTAINER.getPassword() = " + MY_SQL_CONTAINER.getPassword());
-//
-//        System.setProperty("spring.datasource.driver-class-name", MY_SQL_CONTAINER.getDatabaseName());
-//        System.setProperty("spring.datasource.url", MY_SQL_CONTAINER.getJdbcUrl());
-//        System.setProperty("spring.datasource.username", MY_SQL_CONTAINER.getUsername());
-//        System.setProperty("spring.datasource.password", MY_SQL_CONTAINER.getPassword());
--- src/test/java/com/flab/offcoupon/RabbitMQTestInitializer.java
+++ src/test/java/com/flab/offcoupon/RabbitMQTestInitializer.java
@@ -1,36 +1,37 @@
-//package com.flab.offcoupon;
-//
-//import com.rabbitmq.client.BuiltinExchangeType;
-//import com.rabbitmq.client.Channel;
-//import com.rabbitmq.client.Connection;
-//import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-//import org.testcontainers.containers.RabbitMQContainer;
-//
-//import java.io.IOException;
-//import java.util.concurrent.TimeoutException;
-//
-//import static com.flab.offcoupon.util.CouponRabbitMQConstants.*;
-//
-//public class RabbitMQTestInitializer {
-//
-//    static void initializeRabbitMQ(RabbitMQContainer container) {
-//        CachingConnectionFactory factory = new CachingConnectionFactory();
-//        factory.setHost(container.getHost());
-//        factory.setPort(container.getAmqpPort());
-//        factory.setUsername("guest");
-//        factory.setPassword("guest");
-//
-//        try (Connection connection = factory.createConnection(); Channel channel = connection.createChannel()) {
-//            createExchangeAndQueue(channel);
-//        } catch (IOException | TimeoutException e) {
-//            throw new RuntimeException("Failed to create RabbitMQ queue", e);
-//        }
-//    }
-//
-//    static void createExchangeAndQueue(Channel channel) throws IOException {
-//        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
-//        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-//        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
-//    }
-//}
-//
+package com.flab.offcoupon;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.testcontainers.containers.RabbitMQContainer;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import static com.flab.offcoupon.util.CouponRabbitMQConstants.*;
+
+public class RabbitMQTestInitializer {
+
+    static void initializeRabbitMQ(RabbitMQContainer container) {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost(container.getHost());
+        factory.setPort(container.getAmqpPort());
+        factory.setUsername(container.getAdminUsername());
+        factory.setPassword(container.getAdminPassword());
+
+        try (Connection connection = factory.getRabbitConnectionFactory().newConnection()) {
+            Channel channel = connection.createChannel();
+            createExchangeAndQueue(channel);
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException("RabbitMQ queue 생성에 실패했습니다.", e);
+        }
+    }
+
+    static void createExchangeAndQueue(Channel channel) throws IOException {
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+    }
+}
+
