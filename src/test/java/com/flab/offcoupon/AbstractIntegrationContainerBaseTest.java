b086ffec7171495ee8bde43b1658b8ded13df871
sejin park <seijin0722@naver.com>
2024-05-09 21:19:50 +0900

    refactor : 안쓰는 코드 제거

--- src/test/java/com/flab/offcoupon/AbstractIntegrationContainerBaseTest.java
+++ src/test/java/com/flab/offcoupon/AbstractIntegrationContainerBaseTest.java
@@ -20,19 +20,13 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("local")
 public class AbstractIntegrationContainerBaseTest {
-//    @Value("${DB_DATABASE}")
-//    private static String database;
 
-//    @LocalServerPort
-//    int port;
     private static final Logger logger = LoggerFactory.getLogger(AbstractIntegrationContainerBaseTest.class);
     @Container
     static GenericContainer MY_REDIS_CONTAINER =
             new GenericContainer("redis:5.0.3-alpine")
                     .withExposedPorts(6379)
                     .withEnv("REDIS_PASSWORD", "yourRedisPassword"); // 비밀번호 설정
-//    @Container
-//    static MySQLContainer MY_SQL_CONTAINER = new MySQLContainer("mysql:8");
     @Container
     static RabbitMQContainer MY_RABBITMQ_CONTAINER = new RabbitMQContainer("rabbitmq:3.10.7");
 
@@ -41,10 +35,6 @@ public class AbstractIntegrationContainerBaseTest {
         registry.add("spring.data.redis.host", MY_REDIS_CONTAINER::getHost);
         registry.add("spring.data.redis.port", () -> MY_REDIS_CONTAINER.getMappedPort(6379));
         registry.add("spring.data.redis.password", () -> MY_REDIS_CONTAINER.getEnvMap().get("REDIS_PASSWORD"));
-
-//        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
-//        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
-//        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
 
         registry.add("spring.rabbitmq.host", MY_RABBITMQ_CONTAINER::getHost);
         registry.add("spring.rabbitmq.port", () -> MY_RABBITMQ_CONTAINER.getMappedPort(5672));
